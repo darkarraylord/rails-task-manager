ff0fb7ae1d5b8c0866c472490cecdc51567426e6
Gil Lima <gilcpd2@gmail.com>
2017-10-31 15:12:38 +0000

    completed solution on rails.

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,40 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    # form to create tasks
+    @task = Task.new
+  end
+
+  def create
+    Task.create(task_params)
+    redirect_to root_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to root_path
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :description)
+  end
+end
